b00ce6e1de80cfda9d6dc5f1fb153baa32b86734
Oana Sabina Iancu <72797258+211IancuOanaSabina@users.noreply.github.com>
2022-04-17 18:45:54 +0300

    Create TeamModel.ts

---
+++ src/app/models/TeamModel.ts
@@ -0,0 +1,24 @@
+import { JsonProperty, JsonObject } from 'typescript-json-serializer';
+import { BaseModel } from './BaseModel';
+
+@JsonObject()
+export class User extends BaseModel {
+
+  @JsonProperty()
+  country: string = "";
+
+  @JsonProperty()
+  founded: Number = 0;
+
+  @JsonProperty()
+  national: Boolean = false;
+
+  @JsonProperty()
+  logo: string = "";
+
+  @JsonProperty()
+  id: Number = 0;
+
+  @JsonProperty()
+  name: string = "";
+}
