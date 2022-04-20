01e71d17ec781bbceef640a211460afce4860398
Sina Sorin <sorin.sina@mountsoftware.ro>
2022-04-20 11:31:46 +0300

    fixed routes, added ethesrproject providers, working login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { ListBetsComponent } from './oders/list-bets/list-bets.component';
-import { BetsInfoComponent } from './pages/bets-info/bets-info.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './pages/login/login.component';
+import {ListBetsComponent} from './oders/list-bets/list-bets.component';
+import {BetsInfoComponent} from './pages/bets-info/bets-info.component';
+import {Page404Component} from "./page404/page404.component";
 
 
 const routes: Routes = [
-  { path: 'matches', loadChildren: () => import('./matches/matches.module').then(m => m.MatchesModule) },
-  { path: 'bets', component:ListBetsComponent },
-  { path: 'login', component: LoginComponent},
-  { path: 'bets-info', component: BetsInfoComponent}
+  {path: 'matches', loadChildren: () => import('./matches/matches.module').then(m => m.MatchesModule)},
+  {path: 'bets', component: ListBetsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'bets-info', component: BetsInfoComponent},
+  {path: '', redirectTo: '/matches', pathMatch: 'full'},
+  {path: '**', component: Page404Component},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Web3ModalService } from '@mindsorg/web3modal-angular';
-import { ListBetsComponent } from 'src/app/oders/list-bets/list-bets.component';
+import {Component, OnInit} from '@angular/core';
+import {Web3ModalService} from '@mindsorg/web3modal-angular';
+import {Web3Provider} from "@ethersproject/providers";
+import {Store} from "@ngxs/store";
+import {CurrentBetActions} from "../../store/currentBet/current-bet-actions";
 
 
 @Component({
@@ -9,20 +11,22 @@ import { ListBetsComponent } from 'src/app/oders/list-bets/list-bets.component';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
 
-  constructor(public web3ModalService: Web3ModalService ) { }
-
-  
+  constructor(public web3ModalService: Web3ModalService,
+              public store: Store) {
+  }
 
   ngOnInit(): void {
-    this.login();
-
   }
 
-  async login(){
-    const provider = await this.web3ModalService.open();
-  }
+  async login() {
+    const provider = await this.web3ModalService.open() as any;
 
+    const web3provider = new Web3Provider(provider)
+    let accounts = await web3provider.listAccounts();
+    let userAddress = accounts[0];
+    this.store.dispatch(new CurrentBetActions.AddUserAddress(userAddress))
+
+  }
 
 }
